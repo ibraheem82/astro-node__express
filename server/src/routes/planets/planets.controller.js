a00b09f2e72c62403a76be8eb8369eba6ebb2347
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-01-01 13:45:17 +0100

    getAllPlanets function was created  and exported while the planets model was imported.

--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,7 +1,9 @@
 /** @format */
-
-const planets = [];
-
+const planets = require("../../models/plantes.model");
 function getAllPlanets(req, res) {
   res.status(200).json(planets);
 }
+
+module.exports = {
+  getAllPlanets,
+};
