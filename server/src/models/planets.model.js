c0c1356a7e2ed3125f9c9157fb6293c67e04d24e
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-01-01 15:25:24 +0100

    [loadPlanetsData] function was created an d used with promise.

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -20,38 +20,36 @@ function isHabitablePlanet(planet) {
 }
 
 // * The parse() is design to be use with a stream
-fs.createReadStream("kepler_data.csv")
-  // * the pipe() function is meant to connect a  readable stream source to a writeable stream destination
-  // * this will conect the two stream together
-  .pipe(
-    parse({
-      // * these will return each rows in our ccv file, as a javascript object.
-      comment: "#",
-      columns: true,
-    })
-  )
-  // * data from the stream which is an event emitter.
-  .on("data", data => {
-    if (isHabitablePlanet(data)) {
-      habitablePlanets.push(data);
-      // results.push(data);
-    }
-  })
-  .on("error", err => {
-    console.log(err);
-  })
-  .on("end", () => {
-    // console.log(results);
-    console.log(`${habitablePlanets.length} habitable planets found!`);
-    // console.log("done");
-    console.log(`${habitablePlanets}`);
-    // * selecting the kepler name property from each of our planets
-    console.log(
-      habitablePlanets.map(planet => {
-        return planet["kepler_name"];
+function loadPlanetsData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream("kepler_data.csv")
+      // * the pipe() function is meant to connect a  readable stream source to a writeable stream destination
+      // * this will conect the two stream together
+      .pipe(
+        parse({
+          // * these will return each rows in our ccv file, as a javascript object.
+          comment: "#",
+          columns: true,
+        })
+      )
+      // * data from the stream which is an event emitter.
+      .on("data", data => {
+        if (isHabitablePlanet(data)) {
+          habitablePlanets.push(data);
+          // results.push(data);
+        }
       })
-    );
+      .on("error", err => {
+        console.log(err);
+        reject(err);
+      })
+      .on("end", () => {
+        console.log(`${habitablePlanets.length} habitable planets found!`);
+        console.log(`${habitablePlanets}`);
+        resolve();
+      });
   });
+}
 
 module.exports = {
   planets: habitablePlanets,
