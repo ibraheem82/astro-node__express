da299bf76bf52dfdd67bc0f06b431ba3af32a1db
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-01-01 15:31:16 +0100

    loadPlanetsData was imported in the [sever]file and used .

--- server/src/server.js
+++ server/src/server.js
@@ -4,9 +4,14 @@ const http = require("http");
 
 const app = require("./app");
 
+// using the function from our model.
+const { loadPlanetsData } = require("./models/planets.model");
+
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
+
+await loadPlanetsData();
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
