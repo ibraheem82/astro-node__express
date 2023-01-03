1f45656ae31be90470cf03556be0013ce269f4f0
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-01-03 14:51:23 +0100

    planet Router part was done.

--- server/src/routes/planets/planets.router.js
+++ server/src/routes/planets/planets.router.js
@@ -1,4 +1,4 @@
-/** @format */
+
 
 const express = require("express");
 
@@ -6,6 +6,6 @@ const { getAllPlanets } = require("./planets.controller");
 
 const planetsRouter = express.Router();
 
-planetsRouter.get("/planets/", getAllPlanets);
+planetsRouter.get("/planets", getAllPlanets);
 
 module.exports = planetsRouter;
