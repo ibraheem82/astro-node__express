b52fd14c52003508746bf17d41aff3eea5778cb4
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-01-15 14:43:44 +0100

    port number was edited.

--- server/src/server.js
+++ server/src/server.js
@@ -7,7 +7,7 @@ const app = require("./app");
 // using the function from our model.
 const { loadPlanetsData } = require("./models/planets.model");
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000 ;
 
 const server = http.createServer(app);
 async function startServer() {
