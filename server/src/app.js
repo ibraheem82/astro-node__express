8722772f34fb30795bf7b333fa228dbc56eff892
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-01-15 14:42:29 +0100

    express was usde to serve the static file using the [fs] built in function.

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,5 @@
 /** @format */
-
+const path = require('path');
 const express = require("express");
 const cors = require("cors");
 const planetsRouter = require("./routes/planets/planets.router");
@@ -13,6 +13,30 @@ app.use(
   })
 );
 app.use(express.json());
+// will serve all of our public files.
+app.use(express.static(path.join(__dirname, '..', 'public')))
+// app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(__dirname + '/public'));
+// app.use(express.static('public'));
+// app.use('/static', express.static(path.join(__dirname, 'public')))
+app.get('/', (req, res) => {
+  // res.sendFile(path.join(__dirname, '..', 'public'))
+  // res.sendFile(path.resolve('public', 'index.html'))
+  // console.log(path.resolve('public', 'index.html'))
+  // res.sendFile(path.resolve('public', 'index.html'))
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+  // console.log(res.sendFile('./index.html'))
+  // res.sendFile('index.html', { root: __dirname });
+  // res.sendFile("public/index.html", { root: __dirname });
+  // res.sendFile(__dirname + '/index.html');
+  // res.sendFile('index.html');
+  //  res.sendFile('./index.html', {root: 'public'});
+
+});
+
+
+
+
 app.use(planetsRouter);
 
 module.exports = app;
