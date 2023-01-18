ad6754e706705a2db22a7633b536ae196e833633
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-01-18 10:33:27 +0100

    getAllLaunches was returned as a JSON,  FROM the launch.model-> in order to turn all the datas from arrays to json ->objects.

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,11 +1,14 @@
-const {launches} = require('../../models/launches.model');
+// const {launches} = require('../../models/launches.model');
+// const launchesModel = require('../../models/launches.model');
+const {getAllLaunches} = require('../../models/launches.model');
 
 
-function getAllLaunches(req, res) {
-  
-  return res.status(200).json(Array.from(launches.values()));
+function httpGetAllLaunches(req, res) {
+  // return res.status(200).json(Array.from(launches.values()));
+  // return res.status(200).json(Array.from(launchesModel.getAllLaunches()));
+  return res.status(200).json(getAllLaunches());
 }
 
 module.exports = {
-  getAllLaunches,
+  httpGetAllLaunches,
 }
