8d590ab55c330c7fdc8ba21fb8e3b725a81a5b74
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-01-20 03:21:29 +0100

    httpAddNewLaunch() was created and exported, the date was turned to the Javascript real date because the data from the date is string.

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,6 +1,6 @@
 // const {launches} = require('../../models/launches.model');
 // const launchesModel = require('../../models/launches.model');
-const {getAllLaunches} = require('../../models/launches.model');
+const { getAllLaunches, addNewLaunch, } = require('../../models/launches.model');
 
 
 function httpGetAllLaunches(req, res) {
@@ -9,6 +9,16 @@ function httpGetAllLaunches(req, res) {
   return res.status(200).json(getAllLaunches());
 }
 
+function httpAddNewLaunch(req, res) {
+  const launch = req.body;
+
+  launch.launchdate = new Date(launch.launchdate);
+  addNewLaunch(launch);
+  res.status(201).json(launch);
+}
+
+
 module.exports = {
   httpGetAllLaunches,
+  httpAddNewLaunch,
 }
