74ffa7d783491cbfc7df55ab710ce8f6e4ab7342
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-02-08 22:33:35 +0100

    add codes for the post request, and to compare different dates that are passed into the request.

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,8 +1,11 @@
 const request = require('supertest');
+    // getting all our express middleware
 const app = require('../../app');
 describe('Test GET /launches', () => {
   test('It should responde with 200 success', async () => {
     // supertest is taking the app object that is passed in, calling the listen function on that object, it is now allowing us to make request directly  to the HTTP server, using the get method -> get('/launches'), just like the browser fetch function.
+    // getting all our express middleware
+
     const response = await request(app)
       .get('/launches')
       // checking the header match JSON.
@@ -14,7 +17,34 @@ describe('Test GET /launches', () => {
 });
 
 describe('Test POST /launch', () => {
-  test('It should respond with 200 success', () => {
+
+  const completeLaunchData = {
+        mission: 'USS Enterprise',
+        rocket: 'NCC 1701-D',
+        target: 'Kepler-186 f',
+        launchDate: 'January 4, 2046',
+  }
+
+  const launchDataWithoutDate = {
+        mission: 'USS Enterprise',
+        rocket: 'NCC 1701-D',
+        target: 'Kepler-186 f',
+  }
+
+  test('It should respond with 201 created',async () => {
+    // getting all our express middleware
+    const response = await request(app)
+      .post('/launches')
+      // to send some datas
+      .send(completeLaunchData)
+      .expect('Content-Type', /json/)
+      .expect(201);
+    
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+    // To make sure that they are equal.
+    expect(responseDate).toBe(requestDate);
+      expect(response.body).toMatchObject(launchDataWithoutDate);
   });
 
 
