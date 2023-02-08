18a618c7e42f3340c3480e8c5cfb3434277ab5b3
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-02-08 21:57:24 +0100

    [supertest] was required and was required and used with (jest) for the get method and also checking, if the content we ARE get is a json.

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,7 +1,15 @@
+const request = require('supertest');
+const app = require('../../app');
 describe('Test GET /launches', () => {
-  test('It should responde with 200 success', () => {
-    const response = 200;
-    expect(response).toBe(200);
+  test('It should responde with 200 success', async () => {
+    // supertest is taking the app object that is passed in, calling the listen function on that object, it is now allowing us to make request directly  to the HTTP server, using the get method -> get('/launches'), just like the browser fetch function.
+    const response = await request(app)
+      .get('/launches')
+      // checking the header match JSON.
+      // check if it
+      .expect('Content-Type', /json/)
+      .expect(200);
+    // expect(response.statusCode).toBe(200);
   });
 });
 
